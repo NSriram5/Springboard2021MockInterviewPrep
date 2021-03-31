d6b679d309201a75a5e7028e141224ba5b760ec6
Geraldo Gomes <69973423+ggomesneto@users.noreply.github.com>
2021-03-31 14:27:02 -0500

    Create Dropbox_answers.js

---
+++ Dropbox_answers.js
@@ -0,0 +1,48 @@
+//7 keys - 7 letters
+// first letter - key letter
+
+function escapeRoom(keypads, wordList) {
+  let result = [];
+  for (keypad of keypads) {
+    let key = keypad[0];
+    let hash = createHash(keypad);
+    let res = keypadRes(hash, wordList, key);
+
+    result.push(res);
+  }
+
+  function keypadRes(hash, words, key, count = 0, idx = 0) {
+    if (words.length === idx) return count;
+
+    let curWord = words[idx];
+    idx++;
+
+    if (!curWord.includes(key)) return keypadRes(hash, words, key, count, idx);
+
+    for (let i = 0; i < curWord.length; i++) {
+      let letter = curWord[i];
+
+      if (!hash[letter]) return keypadRes(hash, words, key, count, idx);
+    }
+    count++;
+    return keypadRes(hash, words, key, count, idx);
+  }
+
+  function createHash(keypad) {
+    let hash = {};
+    for (let i = 0; i < keypad.length; i++) {
+      let letter = keypad[i];
+      hash[letter] = 1;
+    }
+    return hash;
+  }
+
+  return result;
+}
+
+console.log(
+  escapeRoom(
+    ["AELWXYZ", "AELPXYZ", "AELPSXY", "SAELPRT", "XAEBKSY"],
+    ["APPLE", "PLEAS", "PLEASE"]
+  )
+);
